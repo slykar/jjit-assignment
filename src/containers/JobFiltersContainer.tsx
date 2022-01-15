dca7aad535aa4877366a7fcde5852a77cd50efd0
Sylwester Kardziejonek <sylwester.kardziejonek@gmail.com>
2022-01-15 17:38:06 +0100

    wip: simplify app layout

--- src/containers/JobFiltersContainer.tsx
+++ src/containers/JobFiltersContainer.tsx
@@ -1,5 +1,11 @@
 export default function () {
   return (
-    <h1 className="text-xl font-bold text-gray-900">job filters controls</h1>
+    <header className="bg-white border-b">
+      <div className="w-full py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-xl font-bold text-gray-900">
+          job filters controls
+        </h1>
+      </div>
+    </header>
   );
 }
---
+++ src/containers/NavbarContainer.tsx
@@ -0,0 +1,83 @@
+import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+const user = {
+  name: 'Sylwester',
+  email: 'sylwester.kardziejonek@gmail.com',
+  imageUrl: '/assets/images/sk.jpeg',
+};
+
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+];
+
+export default function NavbarContainer() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 invert"
+                    src="https://geek.justjoin.it/wp-content/uploads/2018/02/logo-jjit-1.png"
+                    alt="Workflow"
+                  />
+                </div>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <button className="max-w-xs rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="mx-1 text-gray-300">{user.name}</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={user.imageUrl}
+                      alt=""
+                    />
+                  </button>
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="pt-4 pb-3 border-t border-gray-700">
+              <div className="flex items-center px-5">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={user.imageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium leading-none text-white">
+                    {user.name}
+                  </div>
+                  <div className="text-sm font-medium leading-none text-gray-400">
+                    {user.email}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
